e6b755885a110cb1d49b7c71f77ed01c3e8336ce
Lorenzo Sapora <lorenzo.sapora@gmail.com>
2022-03-19 01:13:12 +0000

    Update FieldServiceProvider.php

--- src/FieldServiceProvider.php
+++ src/FieldServiceProvider.php
@@ -11,7 +11,7 @@ class FieldServiceProvider extends ServiceProvider
     public function boot()
     {
         Nova::serving(function (ServingNova $event) {
-            Nova::script('format-number', __DIR__ . '/../dist/js/field.js');
+            Nova::script('numbered', __DIR__ . '/../dist/js/field.js');
         });
     }
 
